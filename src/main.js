d39a884103cb76caba385515161f62f140c20695
Danilo Abranches <daniloeabranches@gmail.com>
2019-11-05 20:27:19 -0300

    Buscando da API

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import api from "./api";
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById("repo-form");
+
+    this.inputEl = document.querySelector("input[name=repository]");
 
     this.listEl = document.getElementById("repo-list");
 
@@ -13,15 +17,32 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) {
+      return;
+    }
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url }
+    } = response.data;
+
     this.repositories.push({
-      name: "rocketseat",
-      description: "Tire sua ideia do papel...",
-      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v4",
-      html_url: "https://github.com/rocketseat"
+      name,
+      description,
+      avatar_url,
+      html_url
     });
+
+    this.inputEl.innerHTML = "";
 
     this.render();
   }
@@ -41,6 +62,7 @@ class App {
 
       let linkEl = document.createElement("a");
       linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
       linkEl.appendChild(document.createTextNode("Acessar"));
 
       let listItemEl = document.createElement("li");
