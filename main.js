0b673dbe1586257bc61afec8fa582f7952d25255
Danilo Abranches <daniloeabranches@gmail.com>
2019-11-04 21:06:36 -0300

    Revisando ES6

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-alert("Teste");
+class List {
+  constructor() {
+    this.data = [];
+  }
 
-class Teste {
-  metodo() {}
-
-  outro() {}
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+
+    this.usuario = "Danilo";
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function() {
+  MinhaLista.add("Novo todo");
+};
+
+MinhaLista.mostraUsuario();
